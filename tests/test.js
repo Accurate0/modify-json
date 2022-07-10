1e3a5fc270bfc557bddec2cad99a2c4155dad101
Anurag Singh <contact@anurag.sh>
2022-07-10 19:40:30 +0800

    add reg test

---
+++ tests/test.js
@@ -0,0 +1,18 @@
+var fs = require("fs");
+var diff = require("deep-diff");
+const { exit } = require("process");
+
+var args = process.argv.slice(2);
+if (args.length == 2) {
+    var v1 = JSON.parse(fs.readFileSync(args[0]));
+    var v2 = JSON.parse(fs.readFileSync(args[1]));
+
+    var changes = diff.diff(v1, v2);
+    if (changes) {
+        exit(1);
+    } else {
+        exit(0);
+    }
+} else {
+    exit(123);
+}
